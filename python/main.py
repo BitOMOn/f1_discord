7757bb80271cb71cc64f4b7947b2b50341c67555
Bit-O_MOn <55227123+BitOMOn@users.noreply.github.com>
2023-02-27 20:42:28 +1300

    Update main.py

--- python/main.py
+++ python/main.py
@@ -2,15 +2,22 @@ import requests
 from bs4 import BeautifulSoup
 from discord_webhook import DiscordWebhook
 
-formula1com = requests.get('https://www.formula1.com/en/results.html/2022/drivers.html')
-discordwebhock = "" # put your discord webhook here
+formula1com_drivers = requests.get('https://www.formula1.com/en/results.html/2022/drivers.html')
+formula1com_races = requests.get('https://www.formula1.com/en/results.html/2022/races.html')
+base_url = "https://www.formula1.com"
+discordwebhock = "" # put your discord webhhock here
 
-soup = BeautifulSoup(formula1com.text, 'lxml')
+soup = BeautifulSoup(formula1com_drivers.text, 'lxml')
 pass1 = soup.find('tbody')
 pass2s = pass1.find_all('tr')
 #print(pass2s)
 
 discord_pkt = ""
+
+discord_pkt += "--------------------"
+discord_pkt += "\n"
+discord_pkt += "Drivers:"
+discord_pkt += "\n"
 
 for pass2 in pass2s:
 
@@ -34,7 +41,40 @@ for pass2 in pass2s:
     discord_pkt += text_string
     discord_pkt += "\n"
 
-print(discord_pkt)
+ra_soup = BeautifulSoup(formula1com_races.text, 'lxml')
+ra_pass1 = ra_soup.find('tbody')
+ra_pass2 = ra_pass1.find('tr')
+ra_pass3 = ra_pass2.find('a', class_ = 'dark bold ArchiveLink')
+ra_pass4 = ra_pass3.get('href')
+ra_query_url = base_url + ra_pass4
+print(ra_query_url)
 
+formula1com_race = requests.get(ra_query_url)
+ra1_soup = BeautifulSoup(formula1com_race.text, 'lxml')
+ra1_pass1 = ra1_soup.find('tbody')
+ra1_pass2s = ra1_pass1.find_all('tr')
+#print(ra1_pass2)
+
+discord_pkt += "--------------------"
+discord_pkt += "\n"
+discord_pkt += "Latest Race: "
+discord_pkt += ra_query_url
+discord_pkt += "\n"
+
+for ra1_pass2 in ra1_pass2s:
+
+    ra1_race_POS1 = ra1_pass2.find('td', class_ = 'dark')
+    ra1_race_DRIVER1 = ra1_pass2.find('span', class_ = 'hide-for-mobile')
+
+    ra1_race_POS2 = ra1_race_POS1.get_text()
+    ra1_race_DRIVER2 = ra1_race_DRIVER1.get_text()
+
+    text_string = "P" + ra1_race_POS2 + " " + ra1_race_DRIVER2
+
+    discord_pkt += text_string
+    discord_pkt += "\n"
+
+
+print(discord_pkt)
 webhook = DiscordWebhook(url=discordwebhock, content=discord_pkt)
 webhook.execute()
